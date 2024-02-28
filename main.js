7332b0ff362522ef19af039c74ca22f5a55f3cc2
NuraddinMammadzada <nmammadzada64@gmail.com>
2024-02-28 17:32:01 +0400

    2 yollada cavab cixir

---
+++ main.js
@@ -0,0 +1,18 @@
+// let X = Number(prompt("X-i daxil et:"))
+
+// let Y = Number(prompt("Y-i daxil et:"))
+
+// //IF-Else
+
+// let result;
+// if(X === 50 || Y === 50 || X + Y === 50) {
+//     result = true
+// } else {
+//     result = false;
+// }
+// alert(result);
+
+//?: ile
+
+// let result2 = (X === 50 || Y === 50 || X + Y === 50) ? true : false;
+// alert(result2);
